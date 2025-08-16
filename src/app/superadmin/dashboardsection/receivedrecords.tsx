daccbf86620c710df5608eea749a55ea8d6aeb40
parveen082 <parveenchouhan082@gmail.com>
2025-08-16 12:29:20 +0530

    rtyui

--- src/app/superadmin/dashboardsection/receivedrecords.tsx
+++ src/app/superadmin/dashboardsection/receivedrecords.tsx
@@ -58,7 +58,7 @@ export default function PartnerDashboard() {
     };
 
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-6">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 ">
             {/* Box 1: API Data */}
             <div className="p-6 bg-white rounded-2xl shadow">
                 <h2 className="text-lg font-bold text-blue-700 mb-4">📊 Received from API</h2>
@@ -68,7 +68,7 @@ export default function PartnerDashboard() {
                         onChange={(e) => setPartnerId1(e.target.value)}
                         className="border text-black rounded-lg px-3 py-2"
                     >
-                        <option value="">choose api partner</option>
+                        <option value="">Choose API Partner</option>
                         <option value="Creditsea-keshva">Creditsea-keshva</option>
                     </select>
                     <button
@@ -102,7 +102,7 @@ export default function PartnerDashboard() {
                         onChange={(e) => setPartnerId2(e.target.value)}
                         className="border text-black rounded-lg px-3 py-2"
                     >
-                        <option value="">choose partner</option>
+                        <option value="">Choose CRM Partner</option>
                         <option value="tester007">tester007</option>
                     </select>
                     <button
